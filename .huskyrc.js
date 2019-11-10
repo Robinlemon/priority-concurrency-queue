fa5d65650f09914779c5529e54b8ff9b53617d3d
Robinlemon <lewis-gibson@hotmail.com>
2019-11-10 13:17:47 +0000

    Automated Deploys + Cleans Dist

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,5 @@
 module.exports = {
     hooks: {
-        'pre-commit': 'npm run lint && npm test && npm run build',
+        'pre-commit': 'npm run lint:fix && npm test && npm run clean && npm run build',
     },
 };
